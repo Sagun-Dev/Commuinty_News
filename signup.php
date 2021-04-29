f83bf0a2ba60b8f8ec06d4d65a816d0ff66053a7
Sagun Devkota <be2019se664@gces.edu.np>
2021-04-29 14:10:06 +0545

    css first commit

--- signup.php
+++ signup.php
@@ -2,35 +2,36 @@
 <html>
 <head>
 	<title>Register</title>
+	<link rel="stylesheet" type="text/css" href="./CSS/signup.css">
 </head>
 <body>
-	<form action="./Database/users/register.php" method="post">
-		<label for=fname>First Name: </label>
-		<input type="text" name="fname" id="fname" required>
-		<br><br>
-		<label for=l_name>Last Name: </label>
-		<input type="text" name="lname" id="lname" required>
-		<br><br>
-		<label for=dob>DOB: </label>
-		<input type="date" name="dob" id="dob" required>
-		<br><br>
-		<label>Gender: </label><br>
-		<input type="radio" name="gender" value="male" id="male" required>
-		<label for="male">Male</label><br>
-		<input type="radio" name="gender" value="female" id="female" required>
-		<label for="female">Female</label><br>
-		<input type="radio" name="gender" value="other" id="other" required>
-		<label for="other">Other</label><br>
-		<br><br>
-		<label for=email>Email: </label>
-		<input type="email" name="email" id="email" required>
-		<br><br>
-		<label for=password>Password: </label>
-		<input type="password" name="password" id="password" required>
-		<label id="warning"></label>
-		<br><br>
-		<input type="submit" value="Register">
-		<input type="reset">
-	</form>
+	<div id="out">
+		<div id="signup">
+			<form action="./Database/users/register.php" method="post">
+				<input type="text" name="fname" id="fname" placeholder="First Name" required>
+				<br><br>
+				<input type="text" name="lname" id="lname" placeholder="Last Name" required>
+				<br><br>
+				<label for=dob id="dob_lbl">DOB: </label>
+				<input type="date" name="dob" id="dob" required>
+				<br><br>
+				<label id="gender_lbl">Gender: </label><br>
+				<input type="radio" name="gender" value="male" id="male" required>
+				<label for="male">Male</label><br>
+				<input type="radio" name="gender" value="female" id="female" required>
+				<label for="female">Female</label><br>
+				<input type="radio" name="gender" value="other" id="other" required>
+				<label for="other">Other</label><br>
+				<br><br>
+				<input type="email" name="email" id="email" placeholder="Email" required>
+				<br><br>
+				<input type="password" name="password" id="password" placeholder="Password" required>
+				<label id="warning"></label>
+				<br><br>
+				<input type="submit" value="Register" id="submit"><br><br>
+				<input type="reset" id="reset">
+			</form>
+		</div>
+	</div>
 </body>
 </html>
